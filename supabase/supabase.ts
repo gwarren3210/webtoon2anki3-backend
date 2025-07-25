9d4f334a73a274c9da348d50f39bf60ae091dfb7
gwarren3210 <gavrielewarren@gmail.com>
2025-07-25 02:15:30 -0400

    Remove dbToStudyCard function and related StudyCardDb interface from utils.ts. Update getChapterCards function in supabase.ts to return raw data instead of transformed study cards, simplifying data handling and improving performance.

--- supabase/supabase.ts
+++ supabase/supabase.ts
@@ -21,10 +21,8 @@ import type {
 import {
   convertToSeries,
   convertToChapter,
-  dbToStudyCard,
 } from './studySession/utils';
 import { supabaseUrl } from "./client";
-import { StudyCardDb } from "./studySession/utils";
 
 
 
@@ -733,19 +731,17 @@ export const getChapterCards = api<GetChapterCardsRequest, GetChapterCardsRespon
   path: "/supabase/:seriesSlug/:chapterNumber/cards",
   expose: true,
 }, async ({ chapterNumber, seriesSlug, userId }) => {
-  const { data, error } = await supabase
+  const { data: cards, error } = await supabase
     .rpc('get_chapter_cards', {
       p_user_id: userId,
       p_series_slug: seriesSlug,
       p_chapter_number: chapterNumber,
   });
-  log.info('getChapterCards data', data);
-  const studyCards = data.map(dbToStudyCard)
-  log.info('getChapterCards example', studyCards);
+  log.info('getChapterCards data', cards);
   if (error) {
     throw APIError.internal('Failed to fetch chapter words').withDetails({ error: error.message });
   }
-  return { cards: studyCards };
+  return { cards };
 });
 
 // POST /supabase/cards
